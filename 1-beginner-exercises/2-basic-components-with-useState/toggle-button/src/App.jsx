19b4f2ced99a06a10501777380bbf046994e944c
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-11-16 00:14:11 -0500

    added localStorage on toggle-button exercise

--- 1-beginner-exercises/2-basic-components-with-useState/toggle-button/src/App.jsx
+++ 1-beginner-exercises/2-basic-components-with-useState/toggle-button/src/App.jsx
@@ -1,25 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import './App.css'
 
 import { Power } from "lucide-react";
 
 function App() {
-    const [toggle, setToggle] = useState("Off");
-    const [background, setBackground] = useState("white");
+    const [toggle, setToggle] = useState(localStorage.getItem("toggle") || "Off");
+    const [background, setBackground] = useState(localStorage.getItem("theme") || "black");
+
+    useEffect(() => {
+        localStorage.setItem("theme", background);
+        localStorage.setItem("toggle", toggle);
+    }, [background, toggle]);
 
     return (
         <div
-            className={`flex flex-col w-screen h-screen justify-center items-center ${
-                toggle === "On" ? "bg-white text-black" : ""
-            }`}
+            className={`flex flex-col w-screen h-screen justify-center items-center bg-${background}`}
         >
-            <h1 className="mb-5">{toggle}</h1>
+            <h1 className={`mb-5 ${background === "white" ? "text-black" : "text-white"}`}>{toggle}</h1>
             {/* <div className="flex gap-2 border-1 p-3 rounded-full max-w-fit"> */}
             <button
-                onClick={() =>
-                    toggle === "Off" ? setToggle("On") : setToggle("Off")
-                }
-                className={`${toggle === "On" ? "text-white": ""}`}
+                onClick={() => {
+                    toggle === "Off" ? setToggle("On") : setToggle("Off");
+                    background === "white"
+                        ? setBackground("black")
+                        : setBackground("white");
+                }}
+                className={``}
             >
                 <Power />
             </button>
